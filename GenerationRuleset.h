f61c3ce236b332f519e2d7a11b137864b7e21ba8
AceStructor <45950955+AceStructor@users.noreply.github.com>
2018-12-21 10:03:56 +0100

    Update GenerationRuleset.h
    Changed handling of search or change of cells due to the removal of Cell.h.

--- GenerationRuleset.h
+++ GenerationRuleset.h
@@ -6,48 +6,22 @@
 *	This Class defines all functions a cell item needs to be created properly. The Class generation.h will inherit these functions and they will be implemented in generation.cpp
 *
 *	Author: Patrick Ohlsen
-*	Last Changed: 19.12.2018
+*	Last Changed: 21.12.2018
 */
-
-#include <Cell.h>
 
 class GenerationRuleset
 {
     public:
-        virtual Cell * getUpCell(Cell * a);
-        virtual Cell * getDownCell(Cell * a);
-        virtual Cell * getRightCell(Cell * a);
-        virtual Cell * getLeftCell(Cell * a);
-        virtual Cell * getUpRightCell(Cell * a);
-        virtual Cell * getUpLeftCell(Cell * a);
-        virtual Cell * getDownRightCell(Cell * a);
-        virtual Cell * getDownLeftCell(Cell * a);
-
-        virtual void setUpCell(Cell * a, Cell * b); // Set Up pointer of Cell a to Cell b
-        virtual void setDownCell(Cell * a, Cell * b); //...
-        virtual void setRightCell(Cell * a, Cell * b); //...
-        virtual void setLeftCell(Cell * a, Cell * b); //...
-        virtual void setUpRightCell(Cell * a, Cell * b); //...
-        virtual void setUpLeftCell(Cell * a, Cell * b); //...
-        virtual void setDownRightCell(Cell * a, Cell * b); //...
-        virtual void setDownLeftCell(Cell * a, Cell * b); //...
-
         virtual long getPosX();
         virtual long getPosY();
-
-        virtual void setPosX(long x);
-        virtual void setPosY(long y);
-
-        virtual bool getState();
-        virtual void setState(bool state);
     
-        virtual Cell * findCell(long posX, long posY); //see findCellFlowchart.pdf
-        virtual void createNewLivingCell(Cell * a); //see createNewLivingCellFlowchart.pdf
+        virtual bool findCell(long posX, long posY); //see findCellFlowchart.pdf Returns True if Cell exists.
+	    virtual bool getCellState(long posX, long posY); //returns true for living and false for dead.
+        virtual void createNewLivingCell(long posX, long posY); //see createNewLivingCellFlowchart.pdf
     
-        virtual void killCell(Cell * a); //Sets state to dead. If an adacient cell is living, the memory stays allocated. If all adacient cells are dead, the memory must be cleared AND the function has to be called for ALL adacient cells. The cell then also has to be deleted from row and column list.
-        virtual void connectCells(Cell * a, Cell * b); //Connects the cell a to cell b by calculating the realtive position and setting the pointer on each cell to the other one
-
-        virtual void checkRules(Cell * a); //When planning the next generation, all cells must be tested according to the rules.
+        virtual void killCell(long posX, long posY); //Sets state to dead. If an adacient cell is living, the memory stays allocated. If all adacient cells are dead, the memory must be cleared AND the function has to be called for ALL adacient cells. The cell then also has to be deleted from row and column list.
+        
+        virtual void checkRules(long posX, long posY); //When planning the next generation, all cells must be tested according to the rules.
     protected:
 
     private:
