441f06afee26fc6c4050691f04e0f393da271dce
AceStructor <45950955+AceStructor@users.noreply.github.com>
2018-12-21 13:06:26 +0100

    Update Generation.cpp
    Started Implementation

--- Generation.cpp
+++ Generation.cpp
@@ -1,11 +1,23 @@
 #include "Generation.h"
+#include "RowLinkedList.h"
+#include "ColumnLinkedList.h"
 
 Generation::Generation()
 {
-    //ctor
+    rowLinkedList = new RowLinkedList();
 }
 
 Generation::~Generation()
 {
     //dtor
+}
+
+/*
+* Important Note: I will create an instance of the RowLinkedList in Generation.h, 
+* so if I want to check the position of any Cell, I have to reach into this Class.
+*/
+
+bool Generation::findCell(long posX, long posY, bool genCheck)
+{
+	rowLinkedList.find(posY)->columnDummy
 }
